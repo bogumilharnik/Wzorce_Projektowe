08e5438872c1661df4225e155865babfa0bf2ebe
Jakub Kliś <jakubklis2201@gmail.com>
2021-12-09 15:19:48 +0100

    Początek

---
+++ Pamiątka/Pamiątka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pamiątka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
