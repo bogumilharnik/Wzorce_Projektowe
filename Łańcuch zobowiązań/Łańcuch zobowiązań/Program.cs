839514c585f63a9e8b22cbae8911c3b5ef2e18e7
Jakub Kliś <jakubklis2201@gmail.com>
2022-01-25 11:37:58 +0100

    Koniec

--- Łańcuch zobowiązań/Łańcuch zobowiązań/Program.cs
+++ Łańcuch zobowiązań/Łańcuch zobowiązań/Program.cs
@@ -1,74 +1,118 @@
 ﻿using System;
 using System.Text;
 
-namespace Łańcuch_zobowiązań
+public interface Lancuch
 {
-    public interface Lancuch
-    {
-        void ustawNastepne(Lancuch c);
+    public void ustawNastepne(Lancuch c);
+    public void przetwarzaj(Powiadomienia powiadomienia);
+}
 
-        void przetwarzaj(Powiadomienia powiadomienia);
+public class Powiadomienia
+{
+
+    private int number;
+
+    public Powiadomienia(int number)
+    {
+        this.number = number;
     }
 
-    public class Powiadomienia
+    public int pobierzLiczbe()
     {
-        private int number;
-
-        public Powiadomienia(int number)
-        {
-            this.number = number;
-        }
-
-        public int pobierzLiczbe() => number;
+        return number;
     }
 
-    public class BrakLancuch : Lancuch
+}
+
+public class BrakLancuch : Lancuch
+{
+
+    private Lancuch nastepneWLancuchu;
+
+    public void ustawNastepne(Lancuch c)
     {
-        private Lancuch nastepneWLancuchu;
-
-        public void ustawNastepne(Lancuch c)
-        {
-            nastepneWLancuchu = c;
-        }
-
-        public void przetwarzaj(Powiadomienia powiadomienia)
-        {
-            if (powiadomienia.pobierzLiczbe() <= 0)
-            {
-                Console.WriteLine("Brak powiadomień");
-            }
-            else if (powiadomienia.pobierzLiczbe() <= 5)
-            {
-                Console.WriteLine("Mało powiadomień: " + powiadomienia.pobierzLiczbe());
-            }
-            else
-            {
-                Console.WriteLine("Dużo powiadomień: " + powiadomienia.pobierzLiczbe());
-            }
-        }
+        nastepneWLancuchu = c;
     }
 
-    class Program
+    public void przetwarzaj(Powiadomienia powiadomienia)
     {
-        static void Main(String[] args)
+        if (powiadomienia.pobierzLiczbe() <= 0)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Lancuch l1 = new BrakLancuch();
-
-            int i = 0;
-            l1.przetwarzaj(new Powiadomienia(i));
-            i++;
-
-            l1.przetwarzaj(new Powiadomienia(i));
-
-            i = 12;
-            l1.przetwarzaj(new Powiadomienia(i));
-
-            i = 3;
-            l1.przetwarzaj(new Powiadomienia(i));
-
-            i = 0;
-            l1.przetwarzaj(new Powiadomienia(i));
+            Console.WriteLine("Brak powiadomień");
         }
+        else
+        {
+            nastepneWLancuchu.przetwarzaj(powiadomienia);
+        }
+    }
+}
+public class DuzoLancuch : Lancuch
+{
+
+    private Lancuch nastepneWLancuchu;
+
+    public void ustawNastepne(Lancuch c)
+    {
+        nastepneWLancuchu = c;
+    }
+
+    public void przetwarzaj(Powiadomienia powiadomienia)
+    {
+        if (powiadomienia.pobierzLiczbe() > 5)
+        {
+            Console.WriteLine($"Dużo powiadomień: {powiadomienia.pobierzLiczbe()}");
+        }
+        else
+        {
+            nastepneWLancuchu.przetwarzaj(powiadomienia);
+        }
+    }
+}
+public class MaloLancuch : Lancuch
+{
+
+    private Lancuch nastepneWLancuchu;
+
+    public void ustawNastepne(Lancuch c)
+    {
+        nastepneWLancuchu = c;
+    }
+
+    public void przetwarzaj(Powiadomienia powiadomienia)
+    {
+        if (powiadomienia.pobierzLiczbe() <= 5)
+        {
+            Console.WriteLine($"Mało powiadomień: {powiadomienia.pobierzLiczbe()}");
+        }
+        else
+        {
+            nastepneWLancuchu.przetwarzaj(powiadomienia);
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Lancuch l1 = new BrakLancuch();
+        Lancuch l2 = new MaloLancuch();
+        Lancuch l3 = new DuzoLancuch();
+
+        l1.ustawNastepne(l2);
+        l2.ustawNastepne(l3);
+
+        int i = 0;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i++;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = 12;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = 3;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = 0;
+        l1.przetwarzaj(new Powiadomienia(i));
     }
 }
