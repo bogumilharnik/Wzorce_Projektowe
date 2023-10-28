ac474b1a7dfc7e2869228e75b6bb282e9649ffce
bogumilharnik <117505733+bogumilharnik@users.noreply.github.com>
2023-10-28 16:10:50 +0200

    Update Program.cs
    using System;
     
    public interface ITelewizor {
      int Kanal { get; set; }
      void Wlacz();
      void Wylacz();
      void ZmienKanal(int kanal);
    }
     
    public class LG : ITelewizor {
      public LG(){
        this.Kanal = 1;
      }
      public int Kanal { get; set; }
     
      public void Wlacz() {
        Console.WriteLine("Telewizor LG - włączam się.");
      }
     
      public void Wylacz() {
        Console.WriteLine("Telewizor LG - wyłączam się.");
      }
     
      public void ZmienKanal(int kanal) {
        Console.WriteLine($"Telewizor LG - zmieniam kanał: {kanal}");
        this.Kanal = kanal;
      }
    }
     
    public class TvXiaomi : ITelewizor {
      public TvXiaomi(){
        this.Kanal = 1;
      }
      public int Kanal { get; set; }
     
      public void Wlacz() {
        Console.WriteLine("Telewizor Xiaomi - włączam się.");
      }
     
      public void Wylacz() {
        Console.WriteLine("Telewizor Xiaomi - wyłączam się.");
      }
     
      public void ZmienKanal(int kanal) {
        Console.WriteLine($"Telewizor Xiaomi - zmieniam kanał: {kanal}");
        this.Kanal = kanal;
      }
    }
     
    public abstract class PilotAbstrakcyjny {
      protected ITelewizor tv;
     
      public PilotAbstrakcyjny(ITelewizor tv){
        this.tv = tv;
      }
     
      public void ZmienKanal(int kanal) {
        Console.WriteLine($"Pilot {tv.GetType().Name} - zmienia kanał...");
        tv.ZmienKanal(kanal);
      }
    }
     
    public class PilotHarmony : PilotAbstrakcyjny {
      public PilotHarmony(ITelewizor tv) : base(tv) { }
     
      public void DoWlacz(){
        Console.WriteLine("Pilot Harmony - włącz telewizor...");
        tv.Wlacz();
      }
     
      public void DoWylacz(){
        Console.WriteLine("Pilot Harmony - wyłącz telewizor...");
        tv.Wylacz();
      }
    }
     
    public class PilotLG : PilotAbstrakcyjny {
      public PilotLG(ITelewizor tv) : base(tv) { }
     
      public void DoWylacz(){
        Console.WriteLine("Pilot Philips - wyłącz telewizor...");
        tv.Wylacz();
      }
    }
     
    class MainClass {
      public static void Main (string[] args) {
        ITelewizor tv = new LG();
        PilotHarmony pilotHarmony = new PilotHarmony(tv);
        PilotLG PilotLG = new PilotLG(tv);
     
        pilotHarmony.DoWlacz();
        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
        PilotLG.ZmienKanal(100);
        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
        pilotHarmony.DoWylacz();
      }
    }

--- Most/Most/Program.cs
+++ Most/Most/Program.cs
@@ -1,162 +1,98 @@
-﻿using System;
-using System.Text;
-
-namespace Most
-{
-	public interface ITelewizor
-	{
-
-		int Kanal { get; set; }
-		void Wlacz();
-		void Wylacz();
-		void ZmienKanal(int kanal);
-
-	}
-
-	public class TvLg : ITelewizor
-	{
-
-		public TvLg()
-		{
-			this.Kanal = 1;
-		}
-
-		public int Kanal { get; set; }
-
-		public void Wlacz()
-		{
-			Console.WriteLine("Telewizor LG włączony.");
-		}
-
-		public void Wylacz()
-		{
-			Console.WriteLine("Telewizor LG wyłączony.");
-		}
-
-		public void ZmienKanal(int kanal)
-		{
-			Console.WriteLine("Telewizor LG, kanał:" + kanal);
-			this.Kanal = kanal;
-		}
-	}
-
-	public class TvXiaomi : ITelewizor
-	{
-
-		public TvXiaomi()
-		{
-			this.Kanal = 1;
-		}
-
-		public int Kanal { get; set; }
-
-		public void Wlacz()
-		{
-			Console.WriteLine("Telewizor Xiaomi włączony.");
-		}
-
-		public void Wylacz()
-		{
-			Console.WriteLine("Telewizor Xiaomi wyłączony.");
-		}
-
-		public void ZmienKanal(int kanal)
-		{
-			Console.WriteLine("Telewizor Xioami, kanał:" + kanal);
-			this.Kanal = kanal;
-		}
-	}
-
-	public abstract class PilotAbstrakcyjny
-	{
-
-		private ITelewizor tv;
-
-		public PilotAbstrakcyjny(ITelewizor tv)
-		{
-			this.tv = tv;
-		}
-		public void Wlacz()
-		{
-			tv.Wlacz();
-		}
-
-		public void Wylacz()
-		{
-			tv.Wylacz();
-		}
-
-		public void ZmienKanal(int kanal)
-		{
-			tv.ZmienKanal(kanal);
-		}
-	}
-
-	public class PilotHarmony : PilotAbstrakcyjny
-	{
-
-		public PilotHarmony(ITelewizor tv) : base(tv) { }
-
-		public void DoWlacz()
-		{
-			Console.WriteLine("Pilot Harmony włącza telewizor...");
-			Wlacz();
-		}
-
-		public void DoWylacz()
-		{
-			Console.WriteLine("Pilot Harmony wyłącza telewizor...");
-			Wylacz();
-		}
-
-		public void DoZmienKanal(int kanal)
-		{
-			Console.WriteLine("Pilot Harmony zmienia kanał...");
-			base.ZmienKanal(kanal);
-		}
-	}
-
-	public class PilotPhilips : PilotAbstrakcyjny
-	{
-
-		public PilotPhilips(ITelewizor tv) : base(tv) { }
-
-		public void DoWlacz()
-		{
-			Console.WriteLine("Pilot Philips włącza telewizor...");
-			Wlacz();
-		}
-
-		public void DoWylacz()
-		{
-			Console.WriteLine("Pilot Philips wyłącza telewizor...");
-			Wylacz();
-		}
-
-		public void DoZmienKanal(int kanal)
-		{
-			Console.WriteLine("Pilot Philips zmienia kanał...");
-			base.ZmienKanal(kanal);
-		}
-	}
-
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Console.OutputEncoding = Encoding.UTF8;
-
-			ITelewizor tv = new TvLg();
-
-			PilotHarmony pilotHarmony = new PilotHarmony(tv);
-			PilotPhilips pilotPhilips = new PilotPhilips(tv);
-
-			pilotHarmony.DoWlacz();
-			Console.WriteLine("Kanał: " + tv.Kanal);
-			pilotPhilips.DoZmienKanal(100);
-			Console.WriteLine("Kanał: " + tv.Kanal);
-			pilotHarmony.DoWylacz();
-
-		}
-	}
+using System;
+ 
+public interface ITelewizor {
+  int Kanal { get; set; }
+  void Wlacz();
+  void Wylacz();
+  void ZmienKanal(int kanal);
+}
+ 
+public class LG : ITelewizor {
+  public LG(){
+    this.Kanal = 1;
+  }
+  public int Kanal { get; set; }
+ 
+  public void Wlacz() {
+    Console.WriteLine("Telewizor LG - włączam się.");
+  }
+ 
+  public void Wylacz() {
+    Console.WriteLine("Telewizor LG - wyłączam się.");
+  }
+ 
+  public void ZmienKanal(int kanal) {
+    Console.WriteLine($"Telewizor LG - zmieniam kanał: {kanal}");
+    this.Kanal = kanal;
+  }
+}
+ 
+public class TvXiaomi : ITelewizor {
+  public TvXiaomi(){
+    this.Kanal = 1;
+  }
+  public int Kanal { get; set; }
+ 
+  public void Wlacz() {
+    Console.WriteLine("Telewizor Xiaomi - włączam się.");
+  }
+ 
+  public void Wylacz() {
+    Console.WriteLine("Telewizor Xiaomi - wyłączam się.");
+  }
+ 
+  public void ZmienKanal(int kanal) {
+    Console.WriteLine($"Telewizor Xiaomi - zmieniam kanał: {kanal}");
+    this.Kanal = kanal;
+  }
+}
+ 
+public abstract class PilotAbstrakcyjny {
+  protected ITelewizor tv;
+ 
+  public PilotAbstrakcyjny(ITelewizor tv){
+    this.tv = tv;
+  }
+ 
+  public void ZmienKanal(int kanal) {
+    Console.WriteLine($"Pilot {tv.GetType().Name} - zmienia kanał...");
+    tv.ZmienKanal(kanal);
+  }
+}
+ 
+public class PilotHarmony : PilotAbstrakcyjny {
+  public PilotHarmony(ITelewizor tv) : base(tv) { }
+ 
+  public void DoWlacz(){
+    Console.WriteLine("Pilot Harmony - włącz telewizor...");
+    tv.Wlacz();
+  }
+ 
+  public void DoWylacz(){
+    Console.WriteLine("Pilot Harmony - wyłącz telewizor...");
+    tv.Wylacz();
+  }
+}
+ 
+public class PilotLG : PilotAbstrakcyjny {
+  public PilotLG(ITelewizor tv) : base(tv) { }
+ 
+  public void DoWylacz(){
+    Console.WriteLine("Pilot Philips - wyłącz telewizor...");
+    tv.Wylacz();
+  }
+}
+ 
+class MainClass {
+  public static void Main (string[] args) {
+    ITelewizor tv = new LG();
+    PilotHarmony pilotHarmony = new PilotHarmony(tv);
+    PilotLG PilotLG = new PilotLG(tv);
+ 
+    pilotHarmony.DoWlacz();
+    Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+    PilotLG.ZmienKanal(100);
+    Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+    pilotHarmony.DoWylacz();
+  }
 }
