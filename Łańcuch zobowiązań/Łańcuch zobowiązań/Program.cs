72560d56a74448d0ac9af56e2a430c248a32e1f0
bogumilharnik <117505733+bogumilharnik@users.noreply.github.com>
2024-01-27 15:18:01 +0100

    Update Program.cs - modyfikacja programu
    W wyniku otrzymujemy:
    
    Łańcuch: Małpa > Pies > Wiewiórka > Kot
    Kto chce orzech?
    Wiewiórka zjada orzech.
    Kto chce banan?
    Małpa zjada banan.
    Kto chce mięso?
    Pies zjada mięso.
    Kto chce plasterek szynki?
    Pies zjada plasterek szynki.
    Kto chce lody?
    Nikt nie chce tego zjeść: lody
    
    Podzbiór łańcucha: Wiewiórka > Kot
    Kto chce orzech?
    Wiewiórka zjada orzech.
    Kto chce banan?
    Nikt nie chce tego zjeść: banan
    Kto chce mięso?
    Kot zjada mięso.
    Kto chce plasterek szynki?
    Nikt nie chce tego zjeść: plasterek szynki
    Kto chce lody?
    Nikt nie chce tego zjeść: lody

--- Łańcuch zobowiązań/Łańcuch zobowiązań/Program.cs
+++ Łańcuch zobowiązań/Łańcuch zobowiązań/Program.cs
@@ -1,118 +1,133 @@
-﻿using System;
-using System.Text;
 
-public interface Lancuch
+
+using System;
+using System.Collections.Generic;
+
+namespace LancuchZobowiazan
 {
-    public void ustawNastepne(Lancuch c);
-    public void przetwarzaj(Powiadomienia powiadomienia);
-}
-
-public class Powiadomienia
-{
-
-    private int number;
-
-    public Powiadomienia(int number)
+    public interface IHandler
     {
-        this.number = number;
+        IHandler SetNext(IHandler handler);
+        void Handle(string request);
     }
 
-    public int pobierzLiczbe()
+    public abstract class AbstractHandler : IHandler
     {
-        return number;
-    }
+        private IHandler _nextHandler;
 
-}
-
-public class BrakLancuch : Lancuch
-{
-
-    private Lancuch nastepneWLancuchu;
-
-    public void ustawNastepne(Lancuch c)
-    {
-        nastepneWLancuchu = c;
-    }
-
-    public void przetwarzaj(Powiadomienia powiadomienia)
-    {
-        if (powiadomienia.pobierzLiczbe() <= 0)
+        public IHandler SetNext(IHandler handler)
         {
-            Console.WriteLine("Brak powiadomień");
+            _nextHandler = handler;
+            return _nextHandler;
         }
-        else
+
+        public virtual void Handle(string request)
         {
-            nastepneWLancuchu.przetwarzaj(powiadomienia);
+            if (_nextHandler != null)
+            {
+                _nextHandler.Handle(request);
+            }
+            else
+            {
+                Console.WriteLine($"Nikt nie chce tego zjeść: {request}");
+            }
         }
     }
-}
-public class DuzoLancuch : Lancuch
-{
 
-    private Lancuch nastepneWLancuchu;
-
-    public void ustawNastepne(Lancuch c)
+    public class MonkeyHandler : AbstractHandler
     {
-        nastepneWLancuchu = c;
-    }
-
-    public void przetwarzaj(Powiadomienia powiadomienia)
-    {
-        if (powiadomienia.pobierzLiczbe() > 5)
+        public override void Handle(string request)
         {
-            Console.WriteLine($"Dużo powiadomień: {powiadomienia.pobierzLiczbe()}");
-        }
-        else
-        {
-            nastepneWLancuchu.przetwarzaj(powiadomienia);
+            if (request == "banan")
+            {
+                Console.WriteLine("Małpa zjada banan.");
+            }
+            else
+            {
+                base.Handle(request);
+            }
         }
     }
-}
-public class MaloLancuch : Lancuch
-{
 
-    private Lancuch nastepneWLancuchu;
-
-    public void ustawNastepne(Lancuch c)
+    public class DogHandler : AbstractHandler
     {
-        nastepneWLancuchu = c;
-    }
-
-    public void przetwarzaj(Powiadomienia powiadomienia)
-    {
-        if (powiadomienia.pobierzLiczbe() <= 5)
+        public override void Handle(string request)
         {
-            Console.WriteLine($"Mało powiadomień: {powiadomienia.pobierzLiczbe()}");
-        }
-        else
-        {
-            nastepneWLancuchu.przetwarzaj(powiadomienia);
+            if (request == "szynka" || request == "plasterek szynki")
+            {
+                Console.WriteLine($"Pies zjada {request}.");
+            }
+            else if (request == "mięso")
+            {
+                Console.WriteLine("Pies zjada mięso.");
+            }
+            else
+            {
+                base.Handle(request);
+            }
         }
     }
-}
 
-
-class Program
-{
-    static void Main(String[] args)
+    public class SquirrelHandler : AbstractHandler
     {
-        Console.OutputEncoding = Encoding.UTF8;
-        Lancuch l1 = new BrakLancuch();
-        Lancuch l2 = new MaloLancuch();
-        Lancuch l3 = new DuzoLancuch();
+        public override void Handle(string request)
+        {
+            if (request == "orzech")
+            {
+                Console.WriteLine("Wiewiórka zjada orzech.");
+            }
+            else
+            {
+                base.Handle(request);
+            }
+        }
+    }
 
-        l1.ustawNastepne(l2);
-        l2.ustawNastepne(l3);
+    public class CatHandler : AbstractHandler
+    {
+        public override void Handle(string request)
+        {
+            if (request == "mięso")
+            {
+                Console.WriteLine("Kot zjada mięso.");
+            }
+            else
+            {
+                base.Handle(request);
+            }
+        }
+    }
 
-        int i = 0;
-        l1.przetwarzaj(new Powiadomienia(i));
-        i++;
-        l1.przetwarzaj(new Powiadomienia(i));
-        i = 12;
-        l1.przetwarzaj(new Powiadomienia(i));
-        i = 3;
-        l1.przetwarzaj(new Powiadomienia(i));
-        i = 0;
-        l1.przetwarzaj(new Powiadomienia(i));
+    public class Client
+    {
+        public static void ClientCode(AbstractHandler handler)
+        {
+            foreach (var food in new List<string> { "orzech", "banan", "mięso", "plasterek szynki", "lody" })
+            {
+                Console.WriteLine($"Kto chce {food}?");
+                handler.Handle(food);
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var monkey = new MonkeyHandler();
+            var dog = new DogHandler();
+            var squirrel = new SquirrelHandler();
+            var cat = new CatHandler();
+
+            monkey.SetNext(dog).SetNext(squirrel).SetNext(cat);
+
+            Console.WriteLine("Łańcuch: Małpa > Pies > Wiewiórka > Kot");
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Podzbiór łańcucha: Wiewiórka > Kot");
+            squirrel.SetNext(cat);
+            Client.ClientCode(squirrel);
+        }
     }
 }
